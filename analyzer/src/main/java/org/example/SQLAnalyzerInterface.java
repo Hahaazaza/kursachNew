813b02bdef2b23239d3d0e13ab51fc3671f5f5dc
Hahaazaza <73700273+Hahaazaza@users.noreply.github.com>
2024-12-05 17:44:42 +0300

    Delete analyzer directory

--- analyzer/src/main/java/org/example/SQLAnalyzerInterface.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.List;
-import java.util.concurrent.Callable;
-
-public interface SQLAnalyzerInterface {
-    List<SQLInjectionReport> analyzeFile(String filePath);
-    List<Callable<List<SQLInjectionReport>>> analyzeDirectory(String directoryPath);
-}
